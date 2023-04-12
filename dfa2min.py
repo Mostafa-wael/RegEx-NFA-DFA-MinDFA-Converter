53710bedac8b3f69eb8b653d14ea37d63f45376a
Mostafa-wael <mostafa.w.k000@gmail.com>
2023-04-12 09:26:57 +0200

    min dfa working

--- dfa2min.py
+++ dfa2min.py
@@ -1,69 +1,108 @@
-def dfa2min(dfa):
-    states = dfa.to_dict()
-    symbols = dfa.getSymbols()
-    # remove the final states from dfa
-    states.pop('startingState')
-    groups = []
-    final_states = []
-    non_final_states = []
-    # iterate over states keys and values
-    for key, value in states.items():
-        # the value is a dictionary with the transitions and the isTerminatingState flag
-        if value["isTerminatingState"] == True:
-            final_states.append({key: value})
-        else:
-            non_final_states.append({key: value})
-    groups.append(final_states)
-    groups.append(non_final_states)
-    for g in groups:
-        print(g)
-    print('-------------------------')
-    #---------------------------------------------------------------------------
-    # for each group in groups, check if there is a state that has a transition to a state in another group
-    # if there is, split the group into two groups
-    #---------------------------------------------------------------------------
-    split = True
-    while split:
-        split = False # [[{key1: {a: key1, b: key2}}, {key2: {a: w}}] [{key3: {a: key1, b: key2}}, {}]]
-        for i, group in enumerate(groups): # [{key1: {a: key1, b: key2}}, {key2: {a: key3}}] [{key3: {a: key1, b: key2}}, {}]
-            targetGroups = {}
-            first_state = next(iter(group)) # {key1: {a: key1, b: key2}}
-            for key, value in first_state.items():
-                for symbol in symbols:
-                    if symbol in value:
-                        targetGroups[symbol] = [j for j, group in enumerate(groups) if value[symbol] in getGroupKeys(group)][0] # {key1, key2}
-            splitted_states = []
-            for k, state in enumerate(group):
-                outputGroups = {}
-                for key, value in state.items():
+import graphviz
+
+
+class MIN_DFA:
+    def __init__(self, dfa):
+        self.dfa = dfa
+        self.states = self.dfa2min(dfa)
+    def to_dict(self):
+        return self.states
+    def getGroupKeys(self, group):
+        keys = []
+        for state in group:
+            for key, value in state.items():
+                keys.append(key)
+        return keys
+    def dfa2min(self, dfa):
+        states = dfa.to_dict()
+        symbols = dfa.getSymbols()
+        # remove the final states from dfa
+        states.pop('startingState')
+        groups = []
+        final_states = []
+        non_final_states = []
+        # iterate over states keys and values
+        for key, value in states.items():
+            # the value is a dictionary with the transitions and the isTerminatingState flag
+            if value["isTerminatingState"] == True:
+                final_states.append({key: value})
+            else:
+                non_final_states.append({key: value})
+        groups.append(final_states)
+        groups.append(non_final_states)
+        #---------------------------------------------------------------------------
+        # for each group in groups, check if there is a state that has a transition to a state in another group
+        # if there is, split the group into two groups
+        #---------------------------------------------------------------------------
+        split = True
+        while split:
+            split = False # [[{key1: {a: key1, b: key2}}, {key2: {a: w}}] [{key3: {a: key1, b: key2}}, {}]]
+            for i, group in enumerate(groups): # [{key1: {a: key1, b: key2}}, {key2: {a: key3}}] [{key3: {a: key1, b: key2}}, {}]
+                targetGroups = {}
+                first_state = next(iter(group)) # {key1: {a: key1, b: key2}}
+                for key, value in first_state.items():
                     for symbol in symbols:
                         if symbol in value:
-                            List = [j for j, group in enumerate(groups) if value[symbol] in getGroupKeys(group)]
-                            outputGroups[symbol] = List[0]
-                if outputGroups != targetGroups:
-                    split = True
-                    splitted_states.append(state)
-            if len(splitted_states) > 0:
-                groups.insert(i+1, list(splitted_states))
-                groups[i] = [state for state in group if state not in splitted_states]        
-    newGroups = concat_states(groups)
-    return newGroups
+                            targetGroups[symbol] = [j for j, group in enumerate(groups) if value[symbol] in self.getGroupKeys(group)][0] # {key1, key2}
+                splitted_states = []
+                for k, state in enumerate(group):
+                    outputGroups = {}
+                    for key, value in state.items():
+                        for symbol in symbols:
+                            if symbol in value:
+                                List = [j for j, group in enumerate(groups) if value[symbol] in self.getGroupKeys(group)]
+                                outputGroups[symbol] = List[0]
+                    if outputGroups != targetGroups:
+                        split = True
+                        splitted_states.append(state)
+                if len(splitted_states) > 0:
+                    groups.insert(i+1, list(splitted_states))
+                    groups[i] = [state for state in group if state not in splitted_states]        
+        newGroups = self.concat_states(groups)
+        return newGroups
 
-def concat_states(groups):
-    # result = {'startingState': groups[0][0].keys()}
-    # for g, group in enumerate(groups):
-    #     # for all the states in the group, rename them to the group number
-    #     for state in group:
-    #         for key, value in state.items():
-    #             result[g] = value
-        
-    # return result
-        
-def getGroupKeys(group):
-    keys = []
-    for state in group:
-        for key, value in state.items():
-            keys.append(key)
-    return keys
+
+    def concat_states(self, groups):
+        # create a hashtable for the states with the group number as the key
+        hashTable = {}
+        for g, group in enumerate(groups):
+            # for all the states in the group, rename them to the group number
+            for state in group:
+                for key, value in state.items():
+                    hashTable[key] = str(g)
+        # Create a new dictionary for the new groups
+        newGroups = {'startingState':0}
+        groupCopy = groups.copy()
+        # iterate over the groups
+        for g, group in enumerate(groupCopy):
+            # iterate over the states in the group
+            for state in group:
+                # iterate over the states in the group
+                for key, value in state.items():
+                    # iterate over the transitions in the state
+                    for symbol, next_state in value.items():
+                        # if the next state is in another group, replace it with the group number
+                        if next_state in hashTable:
+                            value[symbol] = str(hashTable[next_state])
+                            newGroups[str(g)] = value
+        return newGroups
+    def visualize(self, name='output/min.gv', view=False):
+        graph = graphviz.Digraph(engine='dot')
+        for state, transitions in self.states.items():
+            if state == 'startingState':
+                continue
+            if transitions['isTerminatingState']:
+                graph.node(state, shape='doublecircle')
+            else:
+                graph.node(state, shape='circle')
+            for char, next_state in transitions.items():
+                if char == 'isTerminatingState':
+                    continue
+                children_states = next_state.split(',')
+                for child in children_states:
+                    graph.edge(state, child, label=char)
+        graph.render("output/min", view=False)
+
+            
 
 
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from regex2nfa import NFA, validate_regex, shunt_yard, postfix_to_nfa
 from nfa2dfa import DFA
-from dfa2min import dfa2min
-import graphviz
+from dfa2min import MIN_DFA
 def main():
     # regex = "([A-Ea-c]+.1)|(2.[0-9]*.K?.[ABC].A.B.C)"
     regex = "([A-Ea-c]+1)|(2[0-9]*K?[ABC]ABC)"
@@ -14,6 +13,8 @@ def main():
     postfix = shunt_yard(regex)
     print("postfix:", postfix)
     # print("expected:", "A|B|C|D|E|a|b|c1+20|1|2|3|4|5|6|7|8|9K*A|B|CABC?|")
+
+    print("----------------------------------------------------------------")
     nfa = postfix_to_nfa(postfix)
     print("NFA: ", nfa.to_dict())
     nfa.visualize(name='output/nfa.gv', view=False)
@@ -22,23 +23,9 @@ def main():
     print("DFA: ", dfa.to_dict())
     dfa.visualize(name='output/dfa.gv', view=False)
     print("----------------------------------------------------------------")
-    min_dfa = dfa2min(dfa)
-    print("Minimized DFA: ", min_dfa)
-    graph = graphviz.Digraph(engine='dot')
-    for state, transitions in min_dfa.items():
-        if state == 'startingState':
-            continue
-        if transitions['isTerminatingState']:
-            graph.node(state, shape='doublecircle')
-        else:
-            graph.node(state, shape='circle')
-        for char, next_state in transitions.items():
-            if char == 'isTerminatingState':
-                continue
-            children_states = next_state.split(',')
-            for child in children_states:
-                graph.edge(state, child, label=char)
-    graph.render("output/min", view=False)
+    min_dfa = MIN_DFA(dfa)
+    print("Minimized DFA: ", min_dfa.to_dict())
+    min_dfa.visualize(name='output/min_dfa.gv', view=False)
 
 
     
